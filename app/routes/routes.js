6b11ea5f00d9980d1bc687ebb944c28440686a76
Greg Quan <gjquan@gmail.com>
2015-05-05 18:15:45 -0700

    add title for index page

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,9 +2,10 @@ module.exports = function(app) {
 
     // =====================================
     // HOME PAGE (with login links) ========
+    // (probably move this into api.js eventually)
     // =====================================
     app.get('/', function(req, res) {
-        res.render('pages/index');
+        res.render('pages/index', { title: 'Zen' });
     });
 
 };
